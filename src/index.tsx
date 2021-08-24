541d8ebdd0fd0523aabafd0edd9a2e8c535370f8
Kalvin <kalvinhart@hotmail.co.uk>
2021-08-24 22:44:54 +0100

    Added GlobalStyle to index.

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import GlobalStyle from "./styles/globalStyles";
 import App from "./App";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
   </ThemeProvider>,
   document.getElementById("root")
